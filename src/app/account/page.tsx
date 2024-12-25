24464900b5743951e0c5335388b8d22c65ad7108
minchodang <minsu910725@gmail.com>
2024-12-25 14:14:06 +0900

    feat: account page

---
+++ src/app/account/page.tsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import { useSession, signIn, signOut } from 'next-auth/react';
+import { useQuery } from '@apollo/client';
+import { GET_MENU } from '@/app/queries/clientQueries';
+import withApollo from '../../utils/withApollo';
+import styled, { ThemeProvider } from 'styled-components';
+import GlobalStyle from '../../components/GlobalStyles';
+import MainMenu from '../../components/MainMenu';
+
+const theme = {
+    colors: {
+        primary: 'rgb(15, 23, 42)',
+    },
+};
+
+const MainContainer = styled.div`
+    margin: 2rem auto;
+    max-width: 120rem;
+    padding: 2rem;
+    width: 100%;
+`;
+
+const PageTitle = styled.h1`
+    color: #ffffff;
+`;
+
+const LoginStatus = styled.p`
+    color: #ffffff;
+`;
+
+const SignInOutButton = styled.button`
+    color: #ffffff;
+    padding: 0.5rem;
+    cursor: pointer;
+    margin: 2rem 0;
+`;
+
+const ContentContainer = styled.div`
+    display: flex;
+    flex-flow: column wrap;
+`;
+
+const Content = styled.p`
+    color: #ffffff;
+    font-size: 1.4rem;
+`;
+
+const ItemContainer = styled.div`
+    display: flex;
+    flex-flow: row nowrap;
+    margin: 2rem 0;
+    border: 0.1rem solid black;
+`;
+
+const ItemDescription = styled.div`
+    margin-left: 1rem;
+`;
+
+const Menu: React.FC = () => {
+    const { data: session, status } = useSession();
+    const { loading, error, data } = useQuery(GET_MENU);
+    const userEmail = session?.user?.email;
+
+    if (status === 'loading') {
+        return <Content>Hang on there...</Content>;
+    }
+
+    if (loading) {
+        return <Content>Loading...</Content>;
+    }
+
+    if (error) {
+        return <Content>Something went wrong</Content>;
+    }
+
+    return (
+        <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <MainMenu />
+            <MainContainer>
+                <PageTitle>Menu</PageTitle>
+                <LoginStatus>{userEmail ? `Signed in as ${userEmail}` : 'Not Signed in'}</LoginStatus>
+                {userEmail ? (
+                    <SignInOutButton onClick={() => signOut()}>Sign out</SignInOutButton>
+                ) : (
+                    <SignInOutButton onClick={() => signIn()}>Sign in</SignInOutButton>
+                )}
+                {!loading && !error && data.menu && (
+                    <ContentContainer>
+                        {data.menu.map((item: any) => (
+                            <ItemContainer key={item.id}>
+                                <ItemDescription>
+                                    <Content>{item.name}</Content>
+                                    <Content>{item.foodType}</Content>
+                                    <Content>{item.description}</Content>
+                                </ItemDescription>
+                            </ItemContainer>
+                        ))}
+                    </ContentContainer>
+                )}
+            </MainContainer>
+        </ThemeProvider>
+    );
+};
+
+export default withApollo(Menu);
