b72e7a23b17a599f2d1861fcaf015839d569f18d
minchodang <minsu910725@gmail.com>
2024-12-25 13:54:24 +0900

    feat: constant data

---
+++ data/menu.ts
@@ -0,0 +1,9 @@
+export const menu = [
+    { id: '1', foodType: 'Drinks', name: 'Latte', description: 'Steamed milk' },
+    {
+        id: '2',
+        foodType: 'Drinks',
+        name: 'Cappuccino',
+        description: 'Espresso',
+    },
+];
