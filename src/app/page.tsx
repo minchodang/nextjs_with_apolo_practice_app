a3c1f732b4b1dd8bd5b60ff3f9f68afea6d1c0fd
minchodang <minsu910725@gmail.com>
2024-12-27 14:18:59 +0900

    feat: not found, styled components 및 관련 import 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,67 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+
+import React from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+import MainMenu from './components/MainMenu';
+import GlobalStyle from './components/GlobalStyles';
+
+const theme = {
+    colors: {
+        primary: 'rgb(15, 23, 42)',
+    },
+};
+
+const MainContainer = styled.div`
+    margin: 2rem auto;
+    width: 100%;
+`;
+
+const CoverHeadingBG = styled.div`
+    margin: 2rem auto;
+    display: flex;
+    flex-flow: column;
+    align-items: center;
+    background-color: rgb(255, 255, 255);
+    color: rgb(6, 95, 70);
+    border-radius: 2rem;
+    padding: 2rem;
+`;
+
+const CoverHeading = styled.h1`
+    text-transform: uppercase;
+`;
+
+const CoverIntro = styled.p`
+    font-size: 1.4rem;
+    margin: 2rem 2rem;
+`;
+
+const Hero = styled.div`
+    margin: 2rem auto;
+    background-image: url('https://res.cloudinary.com/d74fh3kw/image/upload/v1692557430/coffee-restaurant/coffee-shop_zlkf7u.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    background-color: rgb(4, 120, 87);
+    height: 67.5rem;
+    width: 100%;
+`;
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <MainContainer>
+                <MainMenu />
+                <CoverHeadingBG>
+                    <CoverHeading>Summer time is here!</CoverHeading>
+                    <CoverIntro>
+                        Our summer menu has arrived. Freshen up your day with our creamy and delicious coffee range, iced teas, and
+                        mouth-watering snacks.
+                    </CoverIntro>
+                </CoverHeadingBG>
+                <Hero />
+            </MainContainer>
+        </ThemeProvider>
+    );
 }
